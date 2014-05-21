6dd4e5e328e55e21c1e9e238f680e11d9c46a18b
Kenan Sulayman <kenan@sly.mn>
2014-05-21 16:04:32 +0200

    Adds to e6eb1565bd00b43a4526b3fb95d3ad77ddf20e7b.

--- app.js
+++ app.js
@@ -355,7 +355,11 @@ var worker = function () {
                                         }
 
                                         request.session.get("gwAuthed", function (err, val) {
-                                                if ((err || !val) && !~request.url.indexOf("/images/")) {
+                                                if ((err || !val)
+                                                        && !~request.url.indexOf("/images/")
+                                                        && (request.url !== "/css")
+                                                        && (request.url !== "/js")
+                                                        ) {
                                                         if ( request.url !== "/login" )
                                                                 return response.writeHead(302, {
                                                                         Location: "/login"
