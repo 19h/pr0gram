ab1a8a71593a17c518e40500d89ec5c95ecc1351
Kenan Sulayman <kenan@sly.mn>
2014-05-21 16:07:21 +0200

    Meh

--- app.js
+++ app.js
@@ -355,11 +355,7 @@ var worker = function () {
                                         }
 
                                         request.session.get("gwAuthed", function (err, val) {
-                                                if ((err || !val)
-                                                        && !~request.url.indexOf("/images/")
-                                                        && (request.url !== "/css")
-                                                        && (request.url !== "/js")
-                                                        ) {
+                                                if ((err || !val) && !~request.url.indexOf("/images/")) {
                                                         if ( request.url !== "/login" )
                                                                 return response.writeHead(302, {
                                                                         Location: "/login"
